fc8cdb555ff451e873ae81e32f44ffd874e00930
theudas <psodmann@gmail.com>
2017-05-22 19:44:23 +0200

    Update getData.py

--- getData.py
+++ getData.py
@@ -55,11 +55,11 @@ def main():
             'http://image-net.org/api/text/imagenet.synset.geturls?wnid=n03405725', \
             'http://image-net.org/api/text/imagenet.synset.geturls?wnid=n07942152', \
             'http://image-net.org/api/text/imagenet.synset.geturls?wnid=n00021265', \
-            'http://image-net.org/api/text/imagenet.synset.geturls?wnid=n07874259', \
-            'http://image-net.org/api/text/imagenet.synset.geturls?wnid=n07873807', \
-            'http://image-net.org/api/text/imagenet.synset.geturls?wnid=n07679356' ]
+            'http://image-net.org/api/text/imagenet.synset.geturls?wnid=n07690019', \
+            'http://image-net.org/api/text/imagenet.synset.geturls?wnid=n07865105', \
+            'http://image-net.org/api/text/imagenet.synset.geturls?wnid=n07697537' ]
     
-    paths = ['pets', 'furniture', 'people', 'food', 'cheese-pizza', 'pizza', 'bread']
+    paths = ['pets', 'furniture', 'people', 'food', 'frankfurter', 'chili-dog', 'hotdog']
     
     
     store_raw_images(paths, links)
