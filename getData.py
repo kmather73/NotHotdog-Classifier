ec9691c5a2bb0191798cc498d117a209b464d8d9
theudas <psodmann@gmail.com>
2017-05-22 19:39:52 +0200

    made it multithreaded
    using a lot (32) threads simultaneously to load the images faster maxing out my bandwidth.

--- getData.py
+++ getData.py
@@ -3,25 +3,36 @@ import urllib.request
 import cv2
 import os
 import numpy as np
+from multiprocessing.dummy import Pool as ThreadPool 
+import itertools
+
+pic_num = 1
 
 def store_raw_images(paths, links):
-    pic_num = 1
+    global pic_num
     for link, path in zip(links, paths):
         if not os.path.exists(path):
             os.makedirs(path)
         image_urls = str(urllib.request.urlopen(link).read())
         
-        
-        for i in image_urls.split('\\n'):
-            try:                
-                urllib.request.urlretrieve(i, path+"/"+str(pic_num)+".jpg")
-                img = cv2.imread(path+"/"+str(pic_num)+".jpg")             
-                if img is not None:
-                    cv2.imwrite(path+"/"+str(pic_num)+".jpg",img)
-                    pic_num += 1
+        pool = ThreadPool(32)
+        pool.starmap(loadImage, zip(itertools.repeat(path),image_urls.split('\\n'),itertools.count(pic_num))) 
+        pool.close() 
+        pool.join()
+                    
+def loadImage(path,link, counter):
+    global pic_num
+    if pic_num < counter:
+        pic_num = counter+1;
+    try:                
+        urllib.request.urlretrieve(link, path+"/"+str(counter)+".jpg")
+        img = cv2.imread(path+"/"+str(counter)+".jpg")             
+        if img is not None:
+            cv2.imwrite(path+"/"+str(counter)+".jpg",img)
+            print(counter)
 
-            except Exception as e:
-                    print(str(e))  
+    except Exception as e:
+        print(str(e))  
     
 def removeInvalid(dirPaths):
     for dirPath in dirPaths:
@@ -44,11 +55,11 @@ def main():
             'http://image-net.org/api/text/imagenet.synset.geturls?wnid=n03405725', \
             'http://image-net.org/api/text/imagenet.synset.geturls?wnid=n07942152', \
             'http://image-net.org/api/text/imagenet.synset.geturls?wnid=n00021265', \
-            'http://image-net.org/api/text/imagenet.synset.geturls?wnid=n07690019', \
-            'http://image-net.org/api/text/imagenet.synset.geturls?wnid=n07865105', \
-            'http://image-net.org/api/text/imagenet.synset.geturls?wnid=n07697537' ]
+            'http://image-net.org/api/text/imagenet.synset.geturls?wnid=n07874259', \
+            'http://image-net.org/api/text/imagenet.synset.geturls?wnid=n07873807', \
+            'http://image-net.org/api/text/imagenet.synset.geturls?wnid=n07679356' ]
     
-    paths = ['pets', 'furniture', 'people', 'food', 'frankfurter', 'chili-dog', 'hotdog']
+    paths = ['pets', 'furniture', 'people', 'food', 'cheese-pizza', 'pizza', 'bread']
     
     
     store_raw_images(paths, links)
